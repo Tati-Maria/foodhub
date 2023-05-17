06a89404c28b680ebcc6382510457801d69c3710
Maria Tati <mauramaria92@gmail.com>
2023-05-17 23:30:46 +0100

    modified menuCard

--- app/components/ui/MenuCard.tsx
+++ app/components/ui/MenuCard.tsx
@@ -19,18 +19,33 @@ type SafeMenuItem = {
 };
 
 type MenuCardProps = {
-  menuItem: SafeMenuItem;
+  id: string;
+  restaurantId: string;
+  name: string;
+  description: string;
+  menuItem: {
+    menu: string;
+    id: string;
+    name: string;
+    description: string;
+    image: string;
+    price: Decimal;
+    menuId: string;
+    userId: string;
+  }[]
 }
 
-const MenuCard = ({menuItem}: MenuCardProps) => {
+const MenuCard = (
+  {id, restaurantId, name, description, menuItem}: MenuCardProps,
+) => {
   return (
     <section className="my-20">
         <Article className="text-center mb-3 space-y-3 pb-5 border-b border-gray-100/20">
-            <h3 className="text-xl font-bold uppercase">{menuItem.name}</h3>
-            <TextView text={menuItem.description} className="text-gray-300" />
+            <h3 className="text-xl font-bold uppercase">{name}</h3>
+            <TextView text={description} className="text-gray-300" />
             <Link
             className="flex justify-center underline mb-5" 
-            href={`/restaurants/${menuItem.restaurantId}/menus/${menuItem.menuId}/new`}
+            href={`/restaurants/${restaurantId}/menus/${id}/new`}
             >
                 Add Menu Item
             </Link>
@@ -38,7 +53,7 @@ const MenuCard = ({menuItem}: MenuCardProps) => {
         <Grid
         className="my-10"
         >
-          {menuItem.menus.map((item: any) => (
+          {menuItem.map((item: any) => (
             <MenuItemCard
             key={item.id}
             itemId={item.id}
